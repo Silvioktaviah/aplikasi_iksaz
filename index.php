915dc94bce51f8df614ad3c17cce25e6fc4d4029
Silvioktaviah <67631355+Silvioktaviah@users.noreply.github.com>
2020-06-30 22:06:31 +0700

    menambah crip api curl

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
+<?php
+<?php
+function http_request($url){
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, 1);
+    $output = curl_exec($ch); 
+    curl_close($ch);      
+    return $output;
+}
+
+$profile = http_request("https://api-pedatren.nuruljadid.dev/santri");
+$profile = json_decode($profile, TRUE);
+?>
+
+
 <html>
   <head>
     <title>TEST API</title>
-  </head>
+     </head>
   <body>
     <h1>TEST API MENGAMBIL DATA SANTRI PADA API PP NURUL JADID</h1>
     <table width='100%' border='1'>
@@ -10,16 +27,15 @@
          <th>NAMA SANTRI</th>
          <th>KAMAR</th>
       </tr>
+      <?php 
+	        foreach ($profile as $key => $value) {
+       ?>
       <tr>
         <td>1</td>
-        <td>NAMA SANTRI A</td>
+        <td><?php echo $value->message ?></td>
         <td>KAMAR SI A</td>        
       </tr>
-      <tr>
-        <td>2</td>
-        <td>NAMA SANTRI B</td>
-        <td>KAMAR SI B</td>        
-      </tr>
+      <?php } ?>
     </table>
   </body>
 </html>
