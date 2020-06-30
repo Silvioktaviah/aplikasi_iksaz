a5e3f0d18d0d08d16c443b43c8dfc03dcf58ec34
Silvioktaviah <67631355+Silvioktaviah@users.noreply.github.com>
2020-06-30 21:44:41 +0700

    membuat page test api

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>TEST API</title>
+  </head>
+  <body>
+    <h1>TEST API MENGAMBIL DATA SANTRI PADA API PP NURUL JADID</h1>
+    <table width='100%' border='1'>
+      <tr>
+        <th>NO</th>
+         <th>NAMA SANTRI</th>
+         <th>KAMAR</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>NAMA SANTRI A</td>
+        <td>KAMAR SI A</td>        
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>NAMA SANTRI B</td>
+        <td>KAMAR SI B</td>        
+      </tr>
+    </table>
+  </body>
+</html>
